7c256731fbcb6e5ea18d7f4cd396bc1ef9fdfbb5
anoopgeorge418 <anoopgeorge418@gmail.com>
2024-07-25 21:23:26 +0530

    madlibs game project

---
+++ Madlibs/main.py
@@ -0,0 +1,8 @@
+adj = input('Adjective: ')
+verb1 = input('Verb: ')
+verb2 = input('Verb: ')
+famous_person = input('Famous person: ')
+
+mad_libs = f'Computer programming is {adj}! It makes me so excited all the time because I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!'
+
+print(mad_libs)
