6dca024c2d103313ccc8038249503dfb7693a7bf
Anoop George <anoopgeorge418@gmail.com>
2025-04-03 00:37:30 +0530

    Project Final File

---
+++ Band Name Generator/main.py
@@ -0,0 +1,11 @@
+# 1. welcome statement
+print("Welcome to Band Name Generator.")
+
+# 2. Ask the user city name
+city_name = input("What's your city name: ")
+
+# 3. Aks the user pet name
+pet_name = input("What's your pet name: ")
+
+# 4. concatinate it display in the console(terminal)
+print(f"Your Band name is: {city_name + " " + pet_name}")
